482aab780ac38798bf29cb734ea0ab9e3de0d328
deniz <yilmaz.deniz.itu@gmail.com>
2023-12-24 15:12:19 +0300

    ci: return error if totalFailed > 0

--- e2e-run-tests.js
+++ e2e-run-tests.js
@@ -9,6 +9,10 @@ cypress
   .then((results) => {
     // eslint-disable-next-line no-console
     console.log(results)
+    if (results.totalFailed > 0) {
+        console.error('Tests failed. Exiting with an error.');
+        process.exit(1); // Exit with a non-zero status code
+    }
   })
   .catch((err) => {
     // eslint-disable-next-line no-console
