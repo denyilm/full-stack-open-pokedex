2e29c33d57c7d9d96a1cee101e9ae235dcbe6419
deniz <yilmaz.deniz.itu@gmail.com>
2023-12-24 16:37:16 +0300

    fix: enable all the routes

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
+const path = require('path')
 
 // Heroku dynamically sets a port
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
-// app.use('/pokemon/*', pokemonsRouter)
+// Serve your React application for all routes
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
