e1c0c0ea8fb655ca6adfaf9032f09d9a96ae5751
deniz <yilmaz.deniz.itu@gmail.com>
2023-12-24 15:15:49 +0300

    fix: eslint fix

--- e2e-run-tests.js
+++ e2e-run-tests.js
@@ -11,7 +11,7 @@ cypress
     console.log(results)
     if (results.totalFailed > 0) {
         console.error('Tests failed. Exiting with an error.');
-        // eslint-disable-next-line no-console
+        // eslint-disable-next-line no-undef
         process.exit(1); // Exit with a non-zero status code
     }
   })
